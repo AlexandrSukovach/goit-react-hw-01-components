1a5571d6fcb9f4bff818e3a5774944bd1444b713
Aleksandr Sukovach <alexsukovach@gmail.com>
2022-02-16 15:33:57 +0200

    add tasck

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import PaintingList from './components/PaintingList';
 import ColorPicker from './components/ColorPicker/ColorPicker';
 import Alert from './components/Alert/Alert';
-// import Container from './components/Container/Container';
-// import Box from './components/Box/Box';
+import Container from './components/Container/Container';
+import Box from './components/Box/Box';
 import paintings from './paintings.json';
 
 const colorPickerOptions = [
@@ -16,16 +16,16 @@ const colorPickerOptions = [
 
 export default function App() {
   return (
-    <div>
-      {/* <Box type="small" classNames="big red" styles={{ color: '#fff' }} />
-      <Box type="medium" />
-      <Box type="large" /> */}
-    {<Alert text="Шеф все пропало!" type="success" />}
-      {<Alert text="Шеф все пропало!" type="warning" />}
+    <Container>
+      {<Alert text="Шеф все пропало!" type="success" />}
+        {<Alert text="Шеф все пропало!" type="warning" />}
       {<Alert text="Шеф все пропало!" type="error" />}
+      <Box type="small" classNames="big red" styles={{ color: '#fff' }} />
+      <Box type="medium" />
+      <Box type="large" />
       <ColorPicker options={colorPickerOptions} />
       <PaintingList items={paintings} />
-    </div>
+    </Container>
   );
 }
 
