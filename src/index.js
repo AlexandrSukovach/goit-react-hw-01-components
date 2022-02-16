b991e1ab93e0e57d60e077f6d874fa9d9406f516
Aleksandr Sukovach <alexsukovach@gmail.com>
2022-02-16 11:17:01 +0200

    add profil

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from 'components/App';
+import { Profil } from 'components/Profile';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Profil />
   </React.StrictMode>,
   document.getElementById('root')
 );
