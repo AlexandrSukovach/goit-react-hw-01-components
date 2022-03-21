8db2a8a3ded5d1561fde41852739a0015a8366c4
Aleksandr Sukovach <alexsukovach@gmail.com>
2022-03-21 14:05:44 +0200

    21/03/22

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,50 +1,47 @@
 import PropTypes from 'prop-types';
 import s from './Profile.module.css';
+import { FcSynchronize } from 'react-icons/fc';
 
 function Profile({ tag, username, location, avatar, stats }) {
-   return (
-      <div className={s.content}>
-         <div className={s.profile} >
-            <div className={s.description} >
-               <img
-                  src={avatar}
-                  alt="User avatar"
-                  className={s.avatar}
-               />
-               <p className={s.name} >{username}</p>
-               <p className={s.tag} >{tag}@pmarica</p>
-               <p className={s.location} >{location}</p>
-            </div>
-            <ul className={s.stats} >
-               <li>
-                  <span className={s.label} >Followers</span>
-                  <span className={s.quantity} >{stats.followers}</span>
-               </li>
-               <li>
-                  <span className={s.label} >Views</span>
-                  <span className={s.quantity} >{stats.views}</span>
-               </li>
-               <li>
-                  <span className={s.label} >Likes</span>
-                  <span className={s.quantity} >{stats.likes}</span>
-               </li>
-            </ul>
-         </div>
+  return (
+    <div className={s.content}>
+      <div className={s.profile}>
+        <div className={s.description}>
+          <img src={avatar} alt="User avatar" className={s.avatar} />
+          <FcSynchronize size={30} />
+          <p className={s.name}>{username}</p>
+          <p className={s.tag}>{tag}@pmarica</p>
+          <p className={s.location}>{location}</p>
+        </div>
+        <ul className={s.stats}>
+          <li>
+            <span className={s.label}>Followers</span>
+            <span className={s.quantity}>{stats.followers}</span>
+          </li>
+          <li>
+            <span className={s.label}>Views</span>
+            <span className={s.quantity}>{stats.views}</span>
+          </li>
+          <li>
+            <span className={s.label}>Likes</span>
+            <span className={s.quantity}>{stats.likes}</span>
+          </li>
+        </ul>
       </div>
-   );
+    </div>
+  );
 }
 
 Profile.propTypes = {
-   username: PropTypes.string.isRequired,
-   tag: PropTypes.string.isRequired,
-   location: PropTypes.string.isRequired,
-   avatar: PropTypes.string.isRequired,
-   stats: PropTypes.shape({
-      followers: PropTypes.number.isRequired,
-      views: PropTypes.number.isRequired,
-      likes: PropTypes.number.isRequired,
-   }),
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
-
 
 export default Profile;
